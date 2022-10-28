841d08ea15bd2cd6faf8d043e6f71bd004a9b026
Nightowlam <nightowl3.00am@gmail.com>
2022-10-28 22:45:41 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [users, setUsers] = useState([]);
+
+  const handleForm = (e) => {
+    e.preventDefault()
+    const name = e.target.name.value;
+    const email = e.target.email.value;
+    const user = { name, email }
+    console.log(user)
+    e.target.reset("")
+}
+
+
+
+
+
+
+
+
+  useEffect(() => {
+    fetch("http://localhost:5000/users")
+      .then((res) => res.json())
+      .then((data) => setUsers(data));
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <form onSubmit={handleForm} className="form">
+          <input type="text" name="name" id="" placeholder="name" /> <br />
+          <input type="email" name="email" id="" placeholder="email" /> <br />
+          <button type="submit">Submit</button>
+        </form>
+        <h2>Users: {users.length}</h2>
+      </div>
+      <>
+        {users.map((user) => (
+          <p key={user.id}>
+            {user.name} {user.email}
+          </p>
+        ))}
+      </>
     </div>
   );
 }
